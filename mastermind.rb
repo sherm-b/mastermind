debf07c6cc5565709656c8ba0caaf4a318c77645
jeremy-b <jeremy.steven.bess@gmail.com>
2022-09-24 10:48:31 -0700

    Chage comp_code from constant to variable to facilitate generation of a new code

--- mastermind.rb
+++ mastermind.rb
@@ -2,7 +2,7 @@ class Mastermind
   def initialize
     @spaces = ['','','','']
     @colors = ['R', 'G', 'B', 'Y', 'W', 'O', 'P', 'T']
-    @COMP_CODE = @colors.sample(4)
+    @comp_code = @colors.sample(4)
   end
 
   def player_turn
@@ -13,29 +13,29 @@ class Mastermind
   end
 
   def turn_check(player_guess)
-    right_color = 0
-    right_place_and_color = 0
+    :right_color = 0
+    :right_place_and_color = 0
     player_guess.each_with_index do |color, index|
-      if @COMP_CODE.include?(color) && color == @COMP_CODE[index]
-        right_place_and_color += 1
-      elsif @COMP_CODE.include?(color)
-        right_color += 1
+      if @comp_code.include?(color) && color == @comp_code[index]
+        :right_place_and_color += 1
+      elsif @comp_code.include?(color)
+        :right_color += 1
       end
     end
 
-    if right_color == 1
+    if :right_color == 1
       puts "You guessed 1 color correctly, but it was in the wrong spot!"
-    elsif right_color > 1
+    elsif :right_color > 1
       puts "You guessed #{right_color} colors correctly, but they were in the wrong spot!"
     end
 
-    if right_place_and_color == 1
+    if :right_place_and_color == 1
       puts "You guessed 1 color and it's position exactly correctly!"
-    elsif right_place_and_color > 1
+    elsif :right_place_and_color > 1
       puts "You guessed #{right_place_and_color} colors and their positions exactly correctly!"
     end
 
-    if right_color == 0 && right_place_and_color == 0
+    if :right_color == 0 && :right_place_and_color == 0
       puts "You didn't guess any colors or their positions correctly. Really unlucky..."
     end
   end
